cafbdbd8e5c6c972d9918e9724df7d1b9a6364ed
Yash Kadam <kadamyash6112@gmail.com>
2025-08-16 10:30:43 +0530

    Initial Commit

---
+++ war card game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  war card game
+//
+//  Created by Yash Balu Kadam on 16/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ war card gameTests/war_card_gameTests.swift
@@ -0,0 +1,17 @@
+//
+//  war_card_gameTests.swift
+//  war card gameTests
+//
+//  Created by Yash Balu Kadam on 16/08/25.
+//
+
+import Testing
+@testable import war_card_game
+
+struct war_card_gameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
